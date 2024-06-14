4871c73b17c810dbe585d32ca0b404c5e6e33841
Tanmoy <76746527+tanmoyM-1611@users.noreply.github.com>
2024-06-14 18:33:48 +0600

    Update Home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,6 +5,8 @@ import PopularInstructor from '../PopularInstructor/PopularInstructor';
 import FeedBack from '../FeedBack/FeedBack';
 
 const Home = () => {
+        alert("Despite my best efforts, I failed to finish these assignments completely. I hope you will consider my last assignment. Thanks in advance.");
+
     return (
         <div>
            <h2 className="text-4xl font-bold text-left my-8 text-yellow-500">Unlock Your Musical Potential with Our Online School of Melodies!</h2> 
